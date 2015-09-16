e2442c8526543bd5a63470c32d2e5a1b3ca5af25
Garrison Benson <benson.garrison@gmail.com>
2015-09-16 17:07:27 -0400

    Improved admin interface for blog posts

--- forgeblog/admin.py
+++ forgeblog/admin.py
@@ -4,5 +4,7 @@ from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'title', 'text']
+    list_display = ('title', 'pub_date')
+    list_filter = ['pub_date']
 
 admin.site.register(Article, ArticleAdmin)

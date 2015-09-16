b048fb68ebad54c9e128d60be3a8ee6550adc671
Garrison Benson <benson.garrison@gmail.com>
2015-09-16 13:25:56 -0400

    Created Article model.

---
+++ forgeblog/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=50)),
+                ('text', models.TextField()),
+                ('pub_date', models.DateTimeField(verbose_name='date published')),
+            ],
+        ),
+    ]
--- forgeblog/models.py
+++ forgeblog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published')

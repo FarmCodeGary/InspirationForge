a9f7421c9c94249cdf8a1e54d8aeec70daa4125c
Garrison Benson <benson.garrison@gmail.com>
2015-09-23 22:11:28 -0400

    PEP8 fixes.

--- blog/test_models.py
+++ blog/test_models.py
@@ -8,7 +8,7 @@ class ArticleMethodTests(TestCase):
         save() should populate the article's text field with HTML.
         """
         source_text = "Test post text"
-        article = Article(title="test article",source_text=source_text)
+        article = Article(title="test article", source_text=source_text)
         article.save()
-        self.assertEqual(article.text.strip(),"<p>Test post text</p>")
+        self.assertEqual(article.text.strip(), "<p>Test post text</p>")
 

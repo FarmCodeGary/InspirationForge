d1d7b79914a399020e2d87fe8d526a35b6ae5073
Garrison Benson <benson.garrison@gmail.com>
2015-10-02 17:06:12 -0400

    Wrote tests for posting comments.

--- blog/test_views.py
+++ blog/test_views.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import Article
+from .models import Article, Comment
 from .views import ArticleView, IndexView
 
 def create_article(title, source_text="test article text", days_in_past=0):
@@ -32,12 +32,52 @@ class IndexViewTests(TestCase):
 
 class ArticleViewTests(TestCase):
     def test_without_leading_zero_on_month(self):
-        pub_date = datetime.datetime(2013, 9, 15)
+        pub_date = timezone.make_aware(datetime.datetime(2013, 9, 15))
         article = Article.objects.create(title="testarticle", source_text="testbody",
             pub_date=pub_date)
         response = self.client.get(reverse('blog:article',
             args=['2013','9','testarticle']))
         self.assertContains(response, 'testarticle', status_code=200)
         self.assertContains(response, 'testbody')
-
-
+    
+    def test_post_valid_comment(self):
+        """
+        Posting a comment with name and text should work.
+        """
+        article = Article.objects.create(title="testarticle")
+        response = self.client.post(
+            article.get_absolute_url() + "#commentform",
+            { 'name': 'John', 'text': 'Test comment!' },
+            follow = True,
+        )
+        self.assertContains(response, 'Test comment!', status_code=200)
+        self.assertContains(response, 'John')
+    
+    def test_post_comment_without_text(self):
+        """
+        Posting a comment without text should not post the comment and
+        should instead display an error message.
+        """
+        article = Article.objects.create(title="testarticle")
+        response = self.client.post(
+            article.get_absolute_url() + "#commentform",
+            { 'name': 'John', 'text': '' },
+            follow = True,
+        )
+        self.assertContains(response, 'John')
+        self.assertContains(response, 'This field is required.')
+    
+    def test_post_comment_without_name(self):
+        """
+        Posting a comment without a name should not post the comment and should
+        instead display an error message.
+        """
+        article = Article.objects.create(title="testarticle")
+        response = self.client.post(
+            article.get_absolute_url() + "#commentform",
+            { 'name': '', 'text': 'Test comment!' },
+            follow = True,
+        )
+        self.assertContains(response, 'Test comment!')
+        self.assertContains(response, 'This field is required.')
+    

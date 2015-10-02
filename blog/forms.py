0ff9386e5fdd2a9a3061f84d8cddf8622369ab3b
Garrison Benson <benson.garrison@gmail.com>
2015-10-02 15:29:52 -0400

    Improved look and feel of comments form.

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,14 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 from .models import Comment
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'text']
+        widgets = {
+            'name': TextInput(attrs={'placeholder': 'Name'}),
+            'text': Textarea(attrs=
+                {'placeholder': 'Enter your comment here...'}
+            ),
+        }
 

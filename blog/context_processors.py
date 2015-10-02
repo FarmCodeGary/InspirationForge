89addba5ec9ae47a007ae4e8e98c5b39c09dc5be
Garrison Benson <benson.garrison@gmail.com>
2015-10-02 15:39:01 -0400

    Added recent comments functionality for sidebar.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,9 +1,13 @@
 from django.utils import timezone
 
-from .models import Article
+from .models import Article, Comment
 
 def latest_content(request):
     latest_articles = Article.objects.filter(pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
-    return {'latest_articles': latest_articles}
+    latest_comments = Comment.objects.filter(pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+    return {'latest_articles': latest_articles,
+            'latest_comments': latest_comments,
+    }
 

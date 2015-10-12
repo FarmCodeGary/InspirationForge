1b19f48b500c8baa5b8121dc39be495c5448fc5e
Garrison Benson <benson.garrison@gmail.com>
2015-10-12 19:34:17 -0400

    Created basic tag functionality. (no tag cloud yet).

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,18 @@ import markdown
 
 class Tag(models.Model):
     name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=30, unique=True)
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('blog:tag', args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        if self.slug == None or self.slug == "":
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -34,7 +43,7 @@ class Article(models.Model):
         self.text = html
         if self.slug == None or self.slug == "":
             self.slug = slugify(self.title)
-        super(Article, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
 class Comment(models.Model):
     article = models.ForeignKey(Article)
--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
         name='indexpage'),
     url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<slug>[a-z0-9_-]+)/$',
         views.ArticleView.as_view(), name='article'),
+    url(r'^tag/(?P<slug>[a-z0-9_-]+)/$', views.TagView.as_view(),
+        name='tag')
 ]
 

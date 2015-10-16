92011ce741ffc15326b15aa53ee2742bdc8fe41c
Garrison Benson <benson.garrison@gmail.com>
2015-10-16 15:19:20 -0400

    Added lots of documentation.

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,9 @@ from django.core.urlresolvers import reverse
 import markdown
 
 class Tag(models.Model):
+    """
+    Model representing a Tag (used for tagging blog posts with a short phrase).
+    """
     name = models.CharField(max_length=30, unique=True)
     slug = models.SlugField(max_length=30, unique=True)
     
@@ -16,12 +19,21 @@ class Tag(models.Model):
         return reverse('blog:tag', args=[self.slug])
     
     def save(self, *args, **kwargs):
+        """
+        Before saving to the database, automatically generates a slug (if
+        one was not given).
+        """
         if self.slug == None or self.slug == "":
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
 
 class Article(models.Model):
+    """
+    Model representing articles (posts) on the blog.
+    
+    The blog's `source_text` is its text in Markdown format.
+    """
     title = models.CharField(max_length=100)
     source_text = models.TextField()
     text = models.TextField(editable=False)
@@ -40,6 +52,10 @@ class Article(models.Model):
             "{:02d}".format(self.pub_date.month), self.slug])
     
     def save(self, *args, **kwargs):
+        """
+        Before saving, populated the `text` field with HTML generated from the
+        Markdown in the `source_text`.
+        """
         html = markdown.markdown(self.source_text)
         self.text = html
         if self.slug == None or self.slug == "":
@@ -48,11 +64,18 @@ class Article(models.Model):
     
     @staticmethod
     def published_articles():
+        """
+        Returns a QuerySet containing all published articles. An article is
+        considered published if its `pub_date` is not in the the future.
+        """
         return Article.objects.filter(pub_date__lte=timezone.now()
             ).order_by('-pub_date')
 
 
 class Comment(models.Model):
+    """"
+    Model for a comment on a blog article.
+    """
     article = models.ForeignKey(Article)
     name = models.CharField(max_length=30)
     text = models.TextField(verbose_name="Comment")
@@ -70,9 +93,16 @@ class Comment(models.Model):
 
 
 def image_filename(instance, filename):
+    """
+    Generates a filename for storing an image by converting the existing
+    filename to lowercase, converting spaces and underscores to hyphens,
+    and putting it in a subfolder {4-digit year}/{2-digit month} based on
+    the current date.
+    """
     now = timezone.now()
     reformatted = filename.lower().replace(" ","-").replace("_","-")
     return "{}/{:02d}/{}".format(now.year, now.month, reformatted)
+
 
 class Image(models.Model):
     name = models.CharField(max_length=50)
--- blog/views.py
+++ blog/views.py
@@ -10,12 +10,23 @@ from .models import Article, Tag
 from .forms import CommentForm
 
 class IndexView(ListView):
+    """
+    View for listing published blog posts. This view can be used by itself
+    as the blog's main index, subclasses can overrride `get_queryset`,
+    `get_page_title` and `get_page_heading` to show a certain subset of
+    articles (e.g. a tag, an author, or a category).
+    """
     template_name = 'blog/index.html'
     paginate_by = 5
     paginate_orphans = 1
     queryset = Article.published_articles()
     
     def get_context_data(self, **kwargs):
+        """
+        Adds `page_title` and `page_heading` to the template context
+        (obtained by calling `get_page_title` and `get_page_heading`,
+        respectively).
+        """
         context = super().get_context_data(**kwargs)
         page_title = self.get_page_title()
         context['page_title'] = self.get_page_title()
@@ -23,14 +34,30 @@ class IndexView(ListView):
         return context
     
     def get_page_title(self):
+        """
+        Returns None. (This can be overridden in a subclass to give the page
+        a specific HTML title. The actual title will still include the
+        site's title.)
+        """
         return None
     
     def get_page_heading(self):
+        """
+        Returns None. (This can be overridden in a subclass to give the page
+        a specific heading.)
+        """
         return None
 
 
 class TagView(IndexView):
+    """
+    A list view that displays all published blog posts with a given tag.
+    """
     def get_queryset(self):
+        """
+        Uses a keyword argument `slug` to choose a tag, and gets all
+        published articles with the tag.
+        """
         slug = self.kwargs['slug']
         return Article.published_articles().filter(tags__slug__exact=slug)
     
@@ -44,10 +71,18 @@ class TagView(IndexView):
 
 
 class ArticleView(DetailView):
+    """
+    `DetailView` displaying the whole text of an article, plus a Comments
+    section.
+    """
     model = Article
     template_name = 'blog/article.html'
     
     def get_object(self):
+        """
+        Uses keyword arguments `year`, `month` and `slug` to find the correct
+        blog article to display.
+        """
         year = int(self.kwargs['year'])
         month = int(self.kwargs['month'])
         slug = self.kwargs['slug']
@@ -58,11 +93,17 @@ class ArticleView(DetailView):
         )
     
     def get_context_data(self, **kwargs):
+        """
+        Adds a blank `CommentForm` to a context variable named `form`.
+        """
         context = super(DetailView, self).get_context_data(**kwargs)
         context['form'] = CommentForm()
         return context
     
     def post(self, request, *args, **kwargs):
+        """
+        A POST request is used to submit a comment on the given article.
+        """
         self.object = self.get_object()
         form = CommentForm(request.POST)
         try:

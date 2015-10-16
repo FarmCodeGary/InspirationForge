1f601ea5180840ed5a2090cb4da640493880680a
Garrison Benson <benson.garrison@gmail.com>
2015-10-16 15:19:48 -0400

    Added tests for image_filename function.

--- blog/test_models.py
+++ blog/test_models.py
@@ -1,6 +1,8 @@
+import os.path
+
 from django.test import TestCase
 
-from .models import Article
+from .models import Article, image_filename
 
 class ArticleMethodTests(TestCase):
     def test_save_generates_html(self):
@@ -11,4 +13,25 @@ class ArticleMethodTests(TestCase):
         article = Article(title="test article", source_text=source_text)
         article.save()
         self.assertEqual(article.text.strip(), "<p>Test post text</p>")
+
+
+
+
+
+class ImageFilenameTests(TestCase):
+    def test_with_spaces(self):
+        """
+        image_filename should convert spaces to hyphens.
+        """
+        original = "test image.png"
+        result = image_filename(None, original) # The instance shouldn't matter
+        self.assertEqual(os.path.basename(result), "test-image.png")
+    
+    def test_with_underscores(self):
+        """
+        image_filename should convert underscores to hyphens.
+        """
+        original = "test_image.png"
+        result = image_filename(None, original) # The instance shouldn't matter
+        self.assertEqual(os.path.basename(result), "test-image.png")
 

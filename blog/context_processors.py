a45073b755cabd6d81647b4d0036fa4acaa5302c
Garrison Benson <benson.garrison@gmail.com>
2015-12-17 18:36:37 -0500

    Added "Contributors" box in sidebar.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -4,7 +4,7 @@ Context processors for the blog app.
 
 from django.db.models import Count
 
-from .models import Article, Comment, Tag
+from .models import Article, Comment, Tag, Contributor
 
 def latest_content(request):
     """
@@ -16,8 +16,11 @@ def latest_content(request):
     latest_comments = Comment.objects.all().order_by('-pub_date')[:5]
     tags = Tag.objects.annotate(num_articles=Count('article')).order_by(
         '-num_articles')
+    contributors = Contributor.objects.annotate(num_articles=Count('article')
+        ).order_by('-num_articles')
     return {'latest_articles': latest_articles,
             'latest_comments': latest_comments,
             'tags': tags,
+            'contributors': contributors,
     }
 
